f85e7df2cdfc5abd4d073bc04f2807270f9a9909
Charlie <lucharles3@gmail.com>
2021-05-01 11:14:38 -0400

    task item animation

--- components/tasks-list/taskItem.js
+++ components/tasks-list/taskItem.js
@@ -1,5 +1,6 @@
 // dependencies
 import { useState } from 'react';
+import classNames from 'classnames';
 // partials
 import TaskList from './taskList';
 
@@ -8,21 +9,34 @@ const TaskItem = ({
     level,
 }) => {
 
+    /* HOOKS */
     const [ itemLevel, setItemLevel ] = useState( level ); // level to be used for styling
-    const [ loadSubList, setLoadSubList ] = useState ( false ); // dont show sublists on initial render
+    const [ subListActive, setSubListActive ] = useState ( false );
 
     const toggleSubList = () => {
-        setLoadSubList( state => !state );
+        setSubListActive( state => !state );
     }
+
+    /* CLASSNAMES */
+    const taskItemWrapperClasses = classNames( 'task-item-wrapper', subListActive ? 'task-item-active' : 'task-item-not-active' );
 
     return (
         <>
-            <button className='task-item-wrapper' onClick={toggleSubList}>
-                <li className='task-item'>{task}</li>
-            </button>
+            <li className={taskItemWrapperClasses}>
+                <div className='task-item-text'>{task}</div>
+                <div className='task-item-icons-wrapper'>
+                    {
+                        subTaskList && (
+                            <button className='task-item-toggle' onClick={toggleSubList}>
+                                icon
+                            </button>
+                        )
+                    }
+                </div>
+            </li>
             {
-                loadSubList &&
-                subTaskList && <TaskList content={ { taskList: subTaskList } } level={itemLevel + 1} />
+                subListActive && subTaskList && (
+                <TaskList content={ { taskList: subTaskList } } level={itemLevel + 1} /> )
             }
         </>
     );
